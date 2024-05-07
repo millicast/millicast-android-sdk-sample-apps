ebe399220a0984ad23a23a22cfe7c24939b86ee7
Mike Patterson <Mike.Patterson@dolby.com>
2024-05-06 20:57:20 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Millicast Android SDK Sample Apps
+
+Starting off this is for : https://jira.dolby.net/jira/browse/DIOS-5387
+
+It could be used to replace https://github.com/CoSMoSoftware/Millicast-Java-SDK-Android-Sample-App-in-Java
+
+As such it should be written to a public standard even though it's in a private repo right now.
+
+We're also going to write it for 1.8.4 initially, instead of 1.9.0
+
+The Native SDK should be built and published locally for now from the 1.8 branch.
+
+Has Spotless included.
---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.spotless) apply false
+}
+
+subprojects {
+    apply(plugin = "com.diffplug.spotless")
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            ktlint("0.46.0")
+        }
+    }
+}
