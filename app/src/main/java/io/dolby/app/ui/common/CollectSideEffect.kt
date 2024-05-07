6aa3e789a2b082ebab546baaea1812279e08f2c9
Mostafa Ashraf <mshaa@dolby.com>
2024-05-07 15:25:29 +0300

    DIOS-5387 add base state viewModel for MVI and helper sideEffect composable

---
+++ app/src/main/java/io/dolby/app/ui/common/CollectSideEffect.kt
@@ -0,0 +1,29 @@
+package io.dolby.app.ui.common
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
+
+@Composable
+fun <SideEffect> CollectSideEffect(
+    effect: Flow<SideEffect>,
+    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
+    context: CoroutineContext = Dispatchers.Main.immediate,
+    onSideEffect: suspend CoroutineScope.(effect: SideEffect) -> Unit,
+) {
+    LaunchedEffect(effect, LocalLifecycleOwner.current) {
+        lifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            withContext(context) {
+                effect.collect { onSideEffect(it) }
+            }
+        }
+    }
+}
