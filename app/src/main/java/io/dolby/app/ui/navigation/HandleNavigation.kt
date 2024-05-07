519e01645c4f1e92e7fc3539f14a028395c1f7df
Mostafa Ashraf <mshaa@dolby.com>
2024-05-08 02:51:14 +0300

    DIOS-5387 Remove duplicate code

--- app/src/main/java/io/dolby/app/ui/navigation/HandleNavigation.kt
+++ app/src/main/java/io/dolby/app/ui/navigation/HandleNavigation.kt
@@ -1,14 +1,8 @@
 package io.dolby.app.ui.navigation
 
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.ExitTransition
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import io.dolby.app.ui.common.CollectSideEffect
-import io.dolby.app.ui.home.HomeScreen
-import io.dolby.app.ui.subscribe.SubscribeScreen
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -26,34 +20,6 @@ fun HandleNavigation(
             is NavigationContract.NavigationEffect.NavigateBack -> {
                 navController.popBackStack()
             }
-        }
-    }
-    NavHost(
-        navController = navController,
-        startDestination = Screen.HomeScreen.route,
-        enterTransition = {
-            EnterTransition.None
-        },
-        exitTransition = {
-            ExitTransition.None
-        },
-        popExitTransition = {
-            ExitTransition.None
-        },
-        popEnterTransition = {
-            EnterTransition.None
-        }
-    ) {
-        composable(
-            route = Screen.HomeScreen.route
-        ) {
-            HomeScreen(navigationViewModel)
-        }
-
-        composable(
-            route = Screen.SubscribeScreen.route
-        ) {
-            SubscribeScreen(navigationViewModel)
         }
     }
 }
