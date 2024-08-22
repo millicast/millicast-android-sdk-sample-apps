26abc9be6f7d37c43d8350a828fcb6fc1443ee73
Ramesh Prasad <ramesh.prasad@dolby.com>
2024-08-22 11:43:12 +1000

    Upgrade to millicast sdk v2.0.0
    Signed-off-by: Mostafa Ashraf <mostafa.shaaban@dolby.com>

--- app/src/main/java/io/dolby/app/features/subscribe/ui/SubscribeViewModel.kt
+++ app/src/main/java/io/dolby/app/features/subscribe/ui/SubscribeViewModel.kt
@@ -3,20 +3,16 @@ package io.dolby.app.features.subscribe.ui
 import android.util.Log
 import com.millicast.Core
 import com.millicast.subscribers.Credential
-import com.millicast.subscribers.ProjectionData
-import com.millicast.subscribers.state.StreamSourceActivity
+import com.millicast.subscribers.remote.RemoteAudioTrack
+import com.millicast.subscribers.remote.RemoteVideoTrack
 import com.millicast.subscribers.state.SubscriberConnectionState
-import com.millicast.subscribers.state.TrackHolder
-import com.millicast.utils.Queue
 import io.dolby.app.common.MultipleStatesViewModel
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
-class SubscribeViewModel(private val queue: Queue, private val isMultiView: Boolean) :
+class SubscribeViewModel(private val isMultiView: Boolean) :
     MultipleStatesViewModel<SubscribeAction, SubscribeUiState, SubscribeModelState, SubscribeEffect>() {
-    private val streamIdMap = mutableMapOf<String, StreamSourceActivity>()
-
     init {
         updateUiState {
             copy(isMultiView = this@SubscribeViewModel.isMultiView)
@@ -32,22 +28,10 @@ class SubscribeViewModel(private val queue: Queue, private val isMultiView: Bool
         uiState: SubscribeUiState
     ): SubscribeUiState {
         return uiState.copy(
-            sourceVideoTracks = if (state.tracks.isNotEmpty()) filterVideoTrackHolder(state.tracks) else emptyMap(),
-            shouldShowTracks = (state.connectionState == SubscriberConnectionState.Subscribed)
+            sourceVideoTracks = state.tracks,
+            shouldShowTracks = (state.connectionState == SubscriberConnectionState.Subscribed),
+            audioTrack = state.audioTrack
         )
-    }
-
-    private fun filterVideoTrackHolder(tracks: LinkedHashMap<String, List<TrackHolder>>): Map<String, TrackHolder.VideoTrackHolder> {
-        val videoTracksMap = tracks.mapValues {
-            it.value.filterIsInstance<TrackHolder.VideoTrackHolder>().firstOrNull()
-        }
-        val nonNullTracks = linkedMapOf<String, TrackHolder.VideoTrackHolder>()
-        videoTracksMap.forEach { entry ->
-            entry.value?.let {
-                nonNullTracks.putIfAbsent(entry.key, it)
-            }
-        }
-        return nonNullTracks
     }
 
     override fun onUiAction(action: SubscribeAction) {
@@ -55,22 +39,12 @@ class SubscribeViewModel(private val queue: Queue, private val isMultiView: Bool
             is SubscribeAction.Subscribe -> {
                 startSubscription()
             }
-            is SubscribeAction.Pause -> {
-                action.sourceId?.let {
-                    unprojectSource(it)
-                } ?: run {
-                    unprojectAll()
-                }
-            }
-            is SubscribeAction.Resume -> {
-                action.sourceId?.let {
-                    projectSource(it)
-                } ?: run {
-                    projectAll()
-                }
-            }
+
             is SubscribeAction.Disconnect -> {
                 disconnect()
+            }
+
+            else -> {
             }
         }
     }
@@ -94,33 +68,28 @@ class SubscribeViewModel(private val queue: Queue, private val isMultiView: Bool
                 }
             }
             launch {
-                subscriber.tracks.collect { trackHolder ->
-                    if (state.value.tracks.size <= 1) { // Special handling here as it is required to get the main source tracks here as they are projected by default
-                        updateSourceTracks(MAIN_SOURCE_ID, trackHolder)
-                    }
+                subscriber.onRemoteTrack.collect { trackHolder ->
                     when (trackHolder) {
-                        is TrackHolder.AudioTrackHolder -> {
-                            trackHolder.audioTrack.setVolume(1.0)
+                        is RemoteAudioTrack -> {
+                            if (state.value.audioTrack == null || state.value.audioTrack?.isActive == false) {
+                                trackHolder.setVolume(1.0)
+                                updateModelStateAndReduceToUi {
+                                    copy(audioTrack = trackHolder)
+                                }
+                            }
                         }
 
-                        else -> {}
-                    }
-                }
-            }
-            launch {
-                subscriber.state.map { it.streamSourceActivities }
-                    .collect { streamSourceActivities ->
-                        queue.post {
-                            streamSourceActivities.forEach {
-                                projectStreamActivityNewTracks(it)
+                        is RemoteVideoTrack -> {
+                            updateModelStateAndReduceToUi {
+                                tracks.add(trackHolder)
+                                copy(tracks = tracks)
                             }
                         }
                     }
+                }
             }
             subscriber.setCredentials(getCredentials())
             subscriber.connect()
-            subscribe()
-            subscriber.enableStats(true)
             updateModelState {
                 copy(subscriber = subscriber)
             }
@@ -128,7 +97,7 @@ class SubscribeViewModel(private val queue: Queue, private val isMultiView: Bool
     }
 
     private fun getCredentials(): Credential {
-        return if (uiState.value.isMultiView) {
+        return if (isMultiView) {
             Credential(
                 streamName = "multiview",
                 accountId = "k9Mwad",
@@ -144,131 +113,9 @@ class SubscribeViewModel(private val queue: Queue, private val isMultiView: Bool
     }
 
     private suspend fun subscribe() {
-        if (state.value.connectionState == SubscriberConnectionState.Connected) {
+        if (state.value.connectionState != SubscriberConnectionState.Subscribed) {
             Log.i(TAG, "Subscribe")
             state.value.subscriber?.subscribe()
-        }
-    }
-
-    /**
-     * Project all sources. This method serves the case of resuming after triggering a pause.
-     * Whether it was due to coming into foreground mode, or after a user intent to resume
-     * via controller.
-     * Another case also if we paused and we want to resume, so you have to store sourceId & list of
-     * projected tracks.
-     * @see [link](https://docs.dolby.io/streaming-apis/docs/android-getting-started-with-subscribing#6-project-media)
-     */
-    private fun projectAll() {
-        launchDefaultScope {
-            state.value.tracks.forEach { sourceTracks ->
-                val list = arrayListOf<ProjectionData?>()
-
-                // Returning null as it represents the main feed source id
-                val sourceId = if (sourceTracks.key == MAIN_SOURCE_ID) null else sourceTracks.key
-                sourceTracks.value.forEach {
-                    list.add(
-                        ProjectionData(
-                            trackId = it.track.kind.name.lowercase(),
-                            media = it.track.kind.name.lowercase(),
-                            mid = it.mid!!
-                        )
-                    )
-                }
-                state.value.subscriber?.project(sourceId, list)
-            }
-        }
-    }
-
-    /**
-     * Project specific source. This method serves the case of resuming after pausing
-     * @param sourceId The target source to be projected
-     */
-    private fun projectSource(sourceId: String) {
-        val list = arrayListOf<ProjectionData?>()
-        state.value.tracks[sourceId]?.let { tracks ->
-            tracks.forEach {
-                list.add(
-                    ProjectionData(
-                        trackId = it.track.kind.name.lowercase(),
-                        media = it.track.kind.name.lowercase(),
-                        mid = it.mid!!
-                    )
-                )
-            }
-            launchIOScope {
-                state.value.subscriber?.project(sourceId, list)
-            }
-        }
-    }
-
-    /**
-     * Unproject all sources using mid (The media id), So we're gathering our active mediaId list
-     * to unproject while still subscribed.
-     */
-    private fun unprojectAll() {
-        val mediaIdList = state.value.tracks.values.flatMap { trackHolderList ->
-            val list = arrayListOf<String?>()
-            val mediaIdList = trackHolderList.map { it.mid }
-            list.addAll(mediaIdList)
-            list
-        }
-        launchIOScope {
-            if (state.value.connectionState == SubscriberConnectionState.Subscribed) {
-                state.value.subscriber?.unproject(ArrayList(mediaIdList))
-            }
-        }
-    }
-
-    /**
-     * Unproject a specific source
-     * @param sourceId The target source to be unprojected
-     */
-    private fun unprojectSource(sourceId: String) {
-        val list = arrayListOf<String?>()
-        state.value.tracks[sourceId]?.let { tracks ->
-            tracks.forEach {
-                list.add(it.mid)
-            }
-            launchIOScope {
-                state.value.subscriber?.unproject(list)
-            }
-        }
-    }
-
-    /**
-     * This method project every newly added track for every new source. Considering that we don't
-     * Want to project the main feed(Which has a null sourceId) again as it is projected by default.
-     * @param sourceId The target source to be projected
-     */
-    private suspend fun projectStreamActivityNewTracks(streamSourceActivity: StreamSourceActivity) {
-        val sourceId = streamSourceActivity.sourceId ?: MAIN_SOURCE_ID
-        val cached = streamIdMap.getOrPut(
-            sourceId
-        ) { streamSourceActivity.copy(activeTracks = emptyArray()) }
-
-        val cachedTracks = cached.activeTracks.toMutableList()
-        val newTracks = streamSourceActivity.activeTracks.filter { activeTrack ->
-            null == cached.activeTracks.find { it.trackId == activeTrack.trackId }
-        }
-        cachedTracks.addAll(newTracks)
-        streamSourceActivity.copy(activeTracks = cachedTracks.toTypedArray())
-        streamIdMap[sourceId] = streamSourceActivity
-
-        streamSourceActivity.sourceId?.let { // Prevent re-projecting the main source
-            newTracks.forEach {
-                state.value.subscriber?.let { subscriber ->
-                    val newlyAddedLocalTrack = subscriber.addRemoteTrackForResult(it.media)
-                    val projectionList = arrayListOf<ProjectionData?>(
-                        ProjectionData(
-                            trackId = it.trackId,
-                            media = it.media.name.lowercase(),
-                            mid = newlyAddedLocalTrack.mid!!
-                        )
-                    )
-                    subscriber.project(streamSourceActivity.sourceId, projectionList)
-                    updateSourceTracks(sourceId, newlyAddedLocalTrack)
-                }
-            }
         }
     }
 
@@ -277,25 +124,7 @@ class SubscribeViewModel(private val queue: Queue, private val isMultiView: Bool
         state.value.subscriber?.release()
     }
 
-    private fun updateSourceTracks(sourceId: String, trackHolder: TrackHolder) {
-        updateModelStateAndReduceToUi {
-            val tracksCopy = linkedMapOf<String, List<TrackHolder>>()
-            tracksCopy.putAll(this.tracks)
-            tracksCopy.putIfAbsent(sourceId, emptyList())
-            val trackHolderList = tracksCopy[sourceId].orEmpty()
-            val updatedTrackHolderList =
-                if (trackHolderList.indexOfFirst { it.track.kind == trackHolder.track.kind } >= 0) trackHolderList else trackHolderList.plus(
-                    trackHolder
-                ) // Prevent adding same track holder
-            tracksCopy[sourceId] = updatedTrackHolderList
-            Log.i(TAG, "previous sourceTracks ${this.tracks}")
-            Log.i(TAG, "new sourceTracks $tracksCopy")
-            copy(tracks = tracksCopy)
-        }
-    }
-
     companion object {
         private const val TAG = "SubscribeViewModel"
-        private const val MAIN_SOURCE_ID = "CAM1"
     }
 }
--- app/src/main/java/io/dolby/app/features/subscribe/ui/tracks/VideoTrack.kt
+++ app/src/main/java/io/dolby/app/features/subscribe/ui/tracks/VideoTrack.kt
@@ -6,36 +6,32 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import com.millicast.Media
-import com.millicast.devices.track.VideoTrack
-import com.millicast.subscribers.state.TrackHolder
+import com.millicast.subscribers.remote.RemoteVideoTrack
 import com.millicast.video.TextureViewRenderer
 import io.dolby.app.common.ui.fontColor
 import org.webrtc.RendererCommon
 
 @Composable
 fun VideoTrack(
-    sourceTrack: Map.Entry<String, TrackHolder.VideoTrackHolder>,
-    modifier: Modifier = Modifier
-) {
-    VideoTrack(id = sourceTrack.key, sourceTrack = sourceTrack.value.videoTrack, modifier = modifier)
-}
-
-@Composable
-fun VideoTrack(
-    id: String,
-    sourceTrack: VideoTrack,
+    sourceTrack: RemoteVideoTrack,
     modifier: Modifier = Modifier
 ) {
     val context = LocalContext.current
+    val lifecycleOwner = rememberUpdatedState(LocalLifecycleOwner.current)
     val videoRenderer = remember(context) {
         TextureViewRenderer(context).apply {
             init(Media.eglBaseContext, null)
@@ -46,15 +42,39 @@ fun VideoTrack(
         modifier = modifier
             .aspectRatio(16 / 9f)
     ) {
+        DisposableEffect(sourceTrack) {
+            val observer = LifecycleEventObserver { _, event ->
+                when (event) {
+                    Lifecycle.Event.ON_PAUSE -> {
+                        sourceTrack.disableAsync()
+                    }
+
+                    Lifecycle.Event.ON_RESUME -> {
+                        sourceTrack.enableAsync(videoSink = videoRenderer)
+                    }
+
+                    else -> {
+                    }
+                }
+            }
+            val lifecycle = lifecycleOwner.value.lifecycle
+            lifecycle.addObserver(observer)
+            onDispose {
+                lifecycle.removeObserver(observer)
+                sourceTrack.disableAsync()
+            }
+        }
         AndroidView(
-            modifier = Modifier.testTag(id),
+            modifier = Modifier.testTag(sourceTrack.sourceId ?: "CAM1"),
             factory = { videoRenderer },
             update = { view ->
                 view.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT)
-                sourceTrack.setVideoSink(videoRenderer)
             }
         )
-        SourceText(sourceId = id, modifier = Modifier.align(Alignment.TopStart))
+        SourceText(
+            sourceId = sourceTrack.sourceId ?: "CAM1",
+            modifier = Modifier.align(Alignment.TopStart)
+        )
     }
 }
 
